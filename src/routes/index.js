178beed30737dc163f6bddd89a64f70c61ce44ad
LusterPearl <Salamioluwatosin90@gmail.com>
2024-07-08 15:32:25 +0100

    babel

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import dbClient from '../config/db'; // Adjust the path as per your project structure
-import redisClient from '../config/redis'; // Adjust the path as per your project structure
+import dbClient from '../config/db';
+import redisClient from '../config/redis';
 
 const app = express();
 const router = express.Router();
