e85df229b1fe16387a8321162f940cf497f946b0
LusterPearl <Salamioluwatosin90@gmail.com>
2024-07-25 14:49:24 +0100

    paymentordercompleted

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -1,9 +1,6 @@
 import Order from '../models/order.js';
 import Stripe from 'stripe';
 
-// Confirm if the environment variable is being read correctly
-console.log('Stripe Secret Key:', process.env.STRIPE_SECRET_KEY);
-
 const stripe = new Stripe('sk_test_51Pb04SRqiqspksZPLGJjsw6tA0HuWfUj5UL5jvK1dfE3du6xhstk2qbmAtkGXyTJd9VMSxmAiWLNCOaRidG67NDH00PDmFM9KV', {
   apiVersion: '2020-08-27',
 });
